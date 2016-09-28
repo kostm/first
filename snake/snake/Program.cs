901d68adceb68abe1b62a60ce1b48164b652072a
kostm <kostmolochkov@yandex.ru>
2016-09-28 16:21:58 +0500

    добавлен список

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -11,14 +11,29 @@ namespace snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1,3,'*');
-            
             p1.Draw();
 
             Point p2 = new Point(3,4,'@');
-            
-
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
             
             Console.ReadLine();
         }

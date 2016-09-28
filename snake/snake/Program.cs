93d2bf0014473c0576dc9b1fc9b1bda3823df559
kostm <kostmolochkov@yandex.ru>
2016-09-28 15:36:55 +0500

    инкапсуляция

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,16 +10,12 @@ namespace snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 2;
-            p1.sym = '*';
+            Point p1 = new Point(1,3,'*');
+            
             p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 3;
-            p2.y = 4;
-            p2.sym = '@';
+            Point p2 = new Point(3,4,'@');
+            
 
             p2.Draw();
 
